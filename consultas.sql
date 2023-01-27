24808e1e06b35c891d677033fb462c2e8ed03392
luis <lgarciiaruiiz@gmail.com>
2023-01-27 21:33:46 +0100

    se agrega consulta

---
+++ consultas.sql
@@ -0,0 +1,11 @@
+/*
+-------------------------------------------------------------------------------------
+MODELO RELACIONAL
+-------------------------------------------------------------------------------------
+cuatro tipos de mapeo
+-uno a uno
+-uno a munchos
+-munchos a uno
+-munchos a munchos
+*/
+create database escuela;
