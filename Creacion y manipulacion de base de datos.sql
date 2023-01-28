c24699ac4e478cc409df3d604510e888631af052
luis <lgarciiaruiiz@gmail.com>
2023-01-28 01:40:52 +0100

    crear y eliminar base de datos

---
+++ Creacion y manipulacion de base de datos.sql
@@ -0,0 +1,17 @@
+/*
+SQL->lenguaje estructura de consulta
+DDL->lenguaje de deficion de datos ejemplo:create,drop
+DML->Lenguaje de manipulacion de datos ejemplo:insert select
+
+entidad->tablas
+atributos->campos
+registro->tuplas
+cardinalidad->relaciones
+
+*/
+/**crear base de datos*/
+create database prueba1;
+-- para cambiar de contexto
+use prueba1;
+-- borrar base de datos
+drop database prueba1;
