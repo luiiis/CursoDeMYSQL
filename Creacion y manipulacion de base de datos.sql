d5afebb414305a5818a15b2b817bfac503d2303d
luis <lgarciiaruiiz@gmail.com>
2023-01-28 01:53:31 +0100

    crear tablas en mysql

--- Creacion y manipulacion de base de datos.sql
+++ Creacion y manipulacion de base de datos.sql
@@ -15,3 +15,24 @@ create database prueba1;
 use prueba1;
 -- borrar base de datos
 drop database prueba1;
+
+/*
+-------------------------------------------------------------------------------------------------------
+crear tablas
+-------------------------------------------------------------------------------------------------------
+*/
+create database prueba1;
+use prueba1;
+-- crear tabla
+create table t_prueba(
+id_prueba int auto_increment, -- se creea campo solo
+nombre varchar(250),
+peso float,
+comentarios text,
+fecha date,
+primary key(id_prueba)
+);
+-- descripcion de tabla sql
+describe t_prueba;
+-- mostrar todas las tablas de mi base de datos
+show tables;
