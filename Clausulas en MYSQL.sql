1d4514e839f8bca572dd35bdb74c27e49914642f
luis <lgarciiaruiiz@gmail.com>
2023-01-28 03:49:05 +0100

    clausula like de diferentes formas con mysql

--- Clausulas en MYSQL.sql
+++ Clausulas en MYSQL.sql
@@ -20,4 +20,18 @@ Clausula between con mysql
 */
 select*from t_prueba where peso between 60 and 80;
 select*from t_prueba where fecha between '2018-01-01' and '2022-02-02'
+/*
+----------------------------------------------------------------------------------------------------------
+Clausula like con mysql
+----------------------------------------------------------------------------------------------------------
+3 maneras o formas de utilizar like, aunque son 6:or
+--'a%'comienza con la letra a
+--'%a' termina con la letra a
+--'%or%' tiene dentro de la cadena un or
+*/
+select*from t_prueba where nombre like 'l%';
+select*from t_prueba where nombre like '%u';
+select*from t_prueba where nombre like '%u%';
+
+
 
