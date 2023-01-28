664430ee704b285b07fe9f399f11bc9ae6ac5dba
luis <lgarciiaruiiz@gmail.com>
2023-01-28 03:43:13 +0100

    clausula between con mysql

--- Clausulas en MYSQL.sql
+++ Clausulas en MYSQL.sql
@@ -13,3 +13,11 @@ Clausula where con mysql
 */
 select * from t_prueba where id_prueba=1;
 select*from t_prueba where fecha='2022-02-02';
+/*
+----------------------------------------------------------------------------------------------------------
+Clausula between con mysql
+----------------------------------------------------------------------------------------------------------
+*/
+select*from t_prueba where peso between 60 and 80;
+select*from t_prueba where fecha between '2018-01-01' and '2022-02-02'
+
