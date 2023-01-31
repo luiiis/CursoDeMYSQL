71260c5ec12b35fabc3171cee802890c65aa8c0f
luis <lgarciiaruiiz@gmail.com>
2023-01-31 23:18:33 +0100

    multitablas y union

--- Consultas,subconsultas,conuslytas multiples,vistas,procedimientos almacenados y trigger.sql
+++ Consultas,subconsultas,conuslytas multiples,vistas,procedimientos almacenados y trigger.sql
@@ -17,4 +17,14 @@ SUBCONSULTAS
 select*from sakila.film where language_id=1 and rental_rate=2.99;
 
 select*from sakila.film where language_id in(select l.language_id from sakila.language l where l.language_id>1);
-
+/*
+--------------------------------------------------------------
+CONSULTAS MULTITABLAS
+--------------------------------------------------------------
+*/
+select*from sakila.film as f 
+inner join sakila.language as l on f.language_id=l.language_id 
+inner join sakila.inventory as i on f.film_id=i.film_id
+where original_language_id is null;
+-- para unir debe de concider el numero de columnas en ambas tablas
+select film_id from sakila.film union select film_id from sakila.inventory;
