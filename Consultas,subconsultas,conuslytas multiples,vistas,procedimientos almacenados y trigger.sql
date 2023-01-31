8452ee9ea7aac99e16b978b8946f38f080fce6cc
luis <lgarciiaruiiz@gmail.com>
2023-01-31 22:00:12 +0100

    subconsultas

--- Consultas,subconsultas,conuslytas multiples,vistas,procedimientos almacenados y trigger.sql
+++ Consultas,subconsultas,conuslytas multiples,vistas,procedimientos almacenados y trigger.sql
@@ -13,3 +13,8 @@ select*from sakila.category LIMIT 3,9;
 SUBCONSULTAS
 --------------------------------------------------------------
 */
+-- donde sea language =1 y rental_rate=2.99
+select*from sakila.film where language_id=1 and rental_rate=2.99;
+
+select*from sakila.film where language_id in(select l.language_id from sakila.language l where l.language_id>1);
+
